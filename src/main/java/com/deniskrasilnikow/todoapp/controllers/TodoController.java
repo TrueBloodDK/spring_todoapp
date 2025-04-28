83e77985ec3305beae98409feaf5d57f2d68045c
True Hustler <dkaras2016@gmail.com>
2025-04-28 21:10:58 +0500

    implementing add feature

--- src/main/java/com/deniskrasilnikow/todoapp/controllers/TodoController.java
+++ src/main/java/com/deniskrasilnikow/todoapp/controllers/TodoController.java
@@ -2,14 +2,17 @@ package com.deniskrasilnikow.todoapp.controllers;
 
 import com.deniskrasilnikow.todoapp.model.TodoItem;
 import com.deniskrasilnikow.todoapp.repositories.TodoItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
-public class TodoController {
+public class TodoController implements CommandLineRunner {
 
     private final TodoItemRepository todoItemRepository;
 
@@ -19,13 +22,24 @@ public class TodoController {
 
     @GetMapping
     public String index(Model model){
-        todoItemRepository.save(new TodoItem("Bebra1"));
-        todoItemRepository.save(new TodoItem("Bebra2"));
-
         List<TodoItem> allTodos = todoItemRepository.findAll();
 
         model.addAttribute("AllTodos", allTodos);
+        model.addAttribute("newTodo", new TodoItem());
 
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute TodoItem todoItem){
+        todoItemRepository.save(todoItem);
+
+        return "redirect:/";
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        todoItemRepository.save(new TodoItem("Bebra1"));
+        todoItemRepository.save(new TodoItem("Bebra2"));
     }
 }
